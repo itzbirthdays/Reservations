b8506572e26250beb8ef3638ce201045d6c67f23
itzbirthdays <144365541+itzbirthdays@users.noreply.github.com>
2023-09-07 11:03:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,14 @@ function createBalloon() {
     balloon.className = 'balloon';
     let size = Math.random() * 20 + 10; // random size between 10 and 30
     let startPos = Math.random() * 100; // random start position
-    
+    let colors = ['red', 'blue', 'green', 'pink'];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
     balloon.style.width = size + 'px';
     balloon.style.height = size + 'px';
-    balloon.style.borderRadius = '50%';
-    balloon.style.background = 'red'; // Change to desired color or make random
-    balloon.style.position = 'absolute';
-    balloon.style.bottom = '-10%';
     balloon.style.left = startPos + 'vw';
-    balloon.style.animation = `float ${Math.random() * 3 + 5}s linear infinite`;
+    balloon.style.background = randomColor;
+    balloon.style.animationDuration = `${Math.random() * 3 + 5}s`;
 
     document.body.appendChild(balloon);
 }
-
