73c6444e6d16e35b8784d620d770fd87b23da1d1
itzbirthdays <144365541+itzbirthdays@users.noreply.github.com>
2023-09-07 11:33:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     for(let i = 0; i < 50; i++) {
         createBalloon();
+        createStar();
     }
 });
 
 function createBalloon() {
     let balloon = document.createElement('div');
     balloon.className = 'balloon';
-    let size = Math.random() * 20 + 10; // random size between 10 and 30
-    let startPos = Math.random() * 100; // random start position
-    let colors = ['red', 'blue', 'green', 'pink'];
+    let colors = ['red', 'blue', 'green', 'pink', 'yellow'];
     let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    let startPos = Math.random() * 100;
 
-    balloon.style.width = size + 'px';
-    balloon.style.height = size + 'px';
-    balloon.style.left = startPos + 'vw';
     balloon.style.background = randomColor;
+    balloon.style.left = startPos + 'vw';
     balloon.style.animationDuration = `${Math.random() * 3 + 5}s`;
 
     document.body.appendChild(balloon);
+}
+
+function createStar() {
+    let star = document.createElement('div');
+    star.className = 'star';
+    let colors = ['red', 'blue', 'green', 'pink', 'yellow'];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    let startPos = Math.random() * 100;
+
+    star.style.backgroundColor = randomColor;
+    star.style.left = startPos + 'vw';
+    star.style.animationDuration = `${Math.random() * 3 + 5}s`;
+
+    document.body.appendChild(star);
 }
