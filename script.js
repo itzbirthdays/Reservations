9006f889bc4e816f01522f8d3763eecdd271df58
itzbirthdays <144365541+itzbirthdays@users.noreply.github.com>
2023-10-11 14:29:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function adjustScrolling() {
         styleSheet.innerText = animationStyle;
         document.head.appendChild(styleSheet);
 
-        content.style.animation = 'autoscroll 80s linear infinite';
+        content.style.animation = 'autoscroll 120s linear infinite';
     } else {
         content.style.animation = '';
     }
