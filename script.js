cb6e20125d16483739348667d5be8d5cf4ae1c17
itzbirthdays <144365541+itzbirthdays@users.noreply.github.com>
2023-10-11 14:20:23 -0500

    Update script.js
    Fully updated with overfill detection.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
-
 document.querySelector('.current-day').innerText = today;
+
+// Display current time on the top left
+const currentTimeElement = document.getElementById('current-time');
+function updateCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const amOrPm = hours >= 12 ? 'PM' : 'AM';
+    const twelveHourFormat = hours > 12 ? hours - 12 : hours === 0 ? 12 : hours;
+    currentTimeElement.innerText = `${twelveHourFormat}:${minutes.toString().padStart(2, '0')} ${amOrPm}`;
+}
+setInterval(updateCurrentTime, 1000);  // Update every second
 
 document.addEventListener('DOMContentLoaded', function() {
     const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTLFJ-ARR393KF4Z6I2FYsYco265ddxfOd8YA37e5qCg6AJe4VpXUF7OwSulPmPX0SyA2apYW7OumWd/pub?output=csv';
@@ -10,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
         header: true,
         complete: function(results) {
             displayData(results.data.filter(entry => entry.Day === today));
+            adjustScrolling();  // Call after the data is displayed
         }
     });
 
@@ -38,22 +50,37 @@ function generateStars() {
         star.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
         star.style.width = `${(Math.random() * 15) + 5}px`;
         star.style.height = star.style.width;
-        star.style.setProperty('--spin-duration', `${2 + Math.random() * 5}s`); // random duration
-        star.style.setProperty('--spin-amount', `${(Math.random() < 0.5 ? 360 : -360)}deg`); // random direction of spin
+        star.style.setProperty('--spin-duration', `${3 + Math.random() * 7}s`);
+        star.style.setProperty('--spin-amount', `${360 * (Math.random() < 0.5 ? 1 : -1)}deg`);
         document.body.appendChild(star);
     }
 }
-document.addEventListener('DOMContentLoaded', function() {
-    // ... your existing code ...
 
-    // Initialize time display
-    updateTime();
-    setInterval(updateTime, 1000); // Update every second
-});
+function adjustScrolling() {
+    const content = document.querySelector('.content');
+    const contentHeight = content.scrollHeight;
+    const containerHeight = document.querySelector('.container').clientHeight;
 
-function updateTime() {
-    const now = new Date();
-    const timeString = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }); // Formats as e.g. "12:35 PM"
-    document.getElementById('current-time').textContent = timeString;
+    if (contentHeight > containerHeight) {
+        const translateYValue = ((contentHeight - containerHeight) / contentHeight) * 100;
+        const animationStyle = `
+            @keyframes autoscroll {
+                0%, 20%, 80%, 100% {
+                    transform: translateY(0);
+                }
+                50% {
+                    transform: translateY(-${translateYValue}%);
+                }
+            }
+        `;
+
+        const styleSheet = document.createElement("style");
+        styleSheet.type = "text/css";
+        styleSheet.innerText = animationStyle;
+        document.head.appendChild(styleSheet);
+
+        content.style.animation = 'autoscroll 80s linear infinite';
+    } else {
+        content.style.animation = '';
+    }
 }
-
